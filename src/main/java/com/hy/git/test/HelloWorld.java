9b1c194f975b15e6d883b0ada89686ddb9e30a0f
xiaozhenxiao <a_zhen@126.com>
2016-06-03 13:10:35 +0800

    Update HelloWorld.java
    再次测试代码冲突问题

--- src/main/java/com/hy/git/test/HelloWorld.java
+++ src/main/java/com/hy/git/test/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("hello world! ");
 		System.out.println("my code hello world!");
 		System.out.println("other people write code!! ");
+		System.out.println("more people write code!! ");
 	}
 }
